e452a5aadab467b571bbd7feeed1136e17a786d8
xOAviOx <avishuklacode@gmail.com>
2026-03-30 01:01:14 +0530

    Multer setup for user photo

---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+const User = require("./../models/userModel");
+
+const sharp = require("sharp");
+const multer = require("multer");
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image please upload only images", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`${process.cwd()}/public/img/users/${req.file.filename}`);
+
+  next();
+});
