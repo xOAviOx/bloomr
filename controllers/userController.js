1204a092f43b0bf75005d6f815af65767ba3062d
xOAviOx <avishuklacode@gmail.com>
2026-03-30 01:06:16 +0530

    Update me function

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,3 +56,32 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword.",
+        400,
+      ),
+    );
+  }
+
+  // Filter out unwanted fields that are not allowed to be updated
+  const filteredBody = filterObj(req.body, "name", "email");
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  // Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
