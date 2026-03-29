003699241bbe0aded0ba44c7a5243e5488ac89cb
xOAviOx <avishuklacode@gmail.com>
2026-03-30 00:13:24 +0530

    Soe utility functions

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use("/api/v1/users/", userRouter);
+app.use("/api/v1/posts/", postRouter);
+app.use("/", viewRouter);
+app.all(/(.*)/, (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 module.exports = app;
