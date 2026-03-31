bdbf6ce3aecc507ef3e6ed84f3781fc4640f07ed
xOAviOx <avishuklacode@gmail.com>
2026-03-31 19:25:16 +0530

    Sign up functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,3 +13,37 @@ const signToken = (id) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    bio: req.body.bio,
+    passwordChangedAt: req.body.passwordChangedAt,
+  });
+
+  const token = signToken(newUser._id);
+
+  //sending cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    secure: true,
+    httpOnly: true,
+  };
+  cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
+
+  newUser.password = undefined;
+
+  res.status(200).json({
+    status: "success",
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
